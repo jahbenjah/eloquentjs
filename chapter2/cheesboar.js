cd95376c5cd0817cf32be826a19e8be4e1c49d31
Benjamin Camacho <jahbenjah@hotmail.com>
2017-03-28 12:11:38 -0600

    Final solution to chessboard exercise

--- chapter2/cheesboar.js
+++ chapter2/cheesboar.js
@@ -1,19 +1,42 @@
-var size = 8 ;
+
+/*
+Write a program that creates a string that represents an 88 grid, using
+newline characters to separate lines. At each position of the grid there
+is either a space or a “#” character. The characters should form a chess
+board.
+Passing this string to console.log should show something like this:
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+*/
+
+
+var size = 64 ;
 var count = 1;
-var blackSquare="#";
-var whiteSquare="#";
+var black="#";
+var white=" ";
 var line = "";
+
+
 do{
+  //line is even
   if(count % 2 == 0){
-    line = line + whiteSquare + blackSquare;
+    for(var i=0 ;line.length <= size ; i++)
+    line = line + white + black;
     console.log(line);
     line ='';
   }
   else{
-   line = line +  blackSquare + whiteSquare ;
+   for(var i=0 ;line.length <= size; i++)
+      line = line +  black + white ;
    console.log(line);
    line ='';
   } 
   count++;
 }
-while(count<= 8)
+while(count<= size)
